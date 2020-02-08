73fa413345a94766ccea32884ce5ed81e8b446d7
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-02-08 18:14:01 +0900

    Start TIMS project

---
+++ TIMS.py
@@ -0,0 +1,12 @@
+import tkinter
+
+window = tkinter.Tk()
+
+# 윈도우 기본 설정
+window.title('TIMS - 참치봇 통합 관리 시스템')
+window.geometry('960x540+200+200')
+window.resizable(False, False)
+
+#
+
+window.mainloop()
