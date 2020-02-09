71011f11dd668f30391cd976e1185430025f015b
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2020-02-09 23:10:04 +0900

    Remade bot start method

--- TIMS_old.py
+++ TIMS_old.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, font, filedialog, messagebox
 import threading
-import os, platform, sys
-import discord
+import os, platform, shutil, sys
 
 window = tk.Tk()
 
@@ -39,55 +38,68 @@ LabelBy.place(x=110, y=50)
 
 # 봇 토큰 라벨 설정
 LabelBotToken = tk.Label(window, text='봇 토큰:')
-LabelBotToken.place(x=10, y=90)
+LabelBotToken.place(x=10, y=80)
 
 # 봇 토큰 입력칸 설정
 EntryBotToken = ttk.Entry(window, show='●')
-EntryBotToken.place(x=80, y=90, width=162)
+EntryBotToken.place(x=80, y=80, width=162)
+
+# 봇 소스 경로 라벨 설정
+LabelBotSource = tk.Label(window, text='봇 소스:')
+LabelBotSource.place(x=10, y=110)
+
+# 봇 소스 경로 입력칸 설정
+EntryBotSource = ttk.Entry(window)
+vEntryBotSource = tk.StringVar()
+EntryBotSource.config(textvariable=vEntryBotSource)
+EntryBotSource.place(x=80, y=110, width=134)
+
+def openFileDialog():
+    file = tk.filedialog.askopenfilename(title='봇 소스 파일 선택', filetypes=(('Python Script', '*.py'), ('All file', '*.*')))
+    EntryBotSource.delete(0, tk.END)
+    if file:
+        vEntryBotSource.set(file)
+
+# 봇 소스 경로 선택창 열기 버튼
+ButtonBotSource = ttk.Button(window, text='...', command=openFileDialog)
+ButtonBotSource.place(x=217, y=110, width= 26, height=21)
 
 # 봇이 여기서 실질적으로 실행됩니다.
-'''
+def botThread():
     if platform.system() == 'Windows':
         os.system('cd {}'.format(os.getcwd()))
         output = os.popen('python _bot.py').read()
         TextBotLog.config(state='normal')
         TextBotLog.insert(tk.END, output)
         TextBotLog.config(state='normal')
-'''
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(client.user.id)
-    await client.change_presence(status=discord.Status.dnd, activity=discord.Game('InfinieTEAM'))
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('hu'):
-        await message.channel.send('dwdw')
-
-def botThread():
-    client.run(EntryBotToken.get())
 
 # 봇 실행 사전 준비
 def botStart():
     ButtonBotStart.config(state='disabled')
     if EntryBotToken.get():
-        threading.Thread(target=botThread,
-                         daemon=True).start()  # 봇 소스 파일이 실행되는 동안 프로그램 진행이 멈추고 응답 없음이 되므로, 멀티스레딩으로 해결.
+        if EntryBotSource.get():
+            try:
+                shutil.copy(EntryBotSource.get(), './_bot.py')
+                threading.Thread(target=botThread,
+                                 daemon=True).start()  # 봇 소스 파일이 실행되는 동안 프로그램 진행이 멈추고 응답 없음이 되므로, 멀티스레딩으로 해결.
+            except FileNotFoundError:
+                messagebox.showerror('파일 열기 오류', '지정된 경로에 파일이 없거나 경로가 잘못되었습니다.')
+        else:
+            messagebox.showerror('파일 열기 오류', '봇 소스 파일 경로를 입력하세요.')
     else:
         messagebox.showerror('토큰 오류', '토큰을 입력하세요')
+    ButtonBotStart.config(state='enabled')
 
 def botStop():
     pass
 
 # 봇 시작 버튼 설정
 ButtonBotStart = ttk.Button(window, text="봇 시작", command=botStart)
-ButtonBotStart.place(x=10, y=118, width=113)
+ButtonBotStart.place(x=10, y=140, width=113)
 
 # 봇 정지 버튼 설정
 ButtonBotStop = ttk.Button(window, text="봇 정지", command=botStop)
-ButtonBotStop.place(x=130, y=118, width=113)
+ButtonBotStop.place(x=130, y=140, width=113)
 
 # 창이 닫힐 때 알림상자.
 def onClosing():
--- _bot.py
+++ _bot.py
@@ -11,5 +11,3 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('hu'):
         await message.channel.send('dwdw')
-    
-client.run('NTE2MTY3NDY4NjEyNDUyMzc5.Xj-KwQ.uY8vGD5C3QkaA_qT2xoVZNW-8BU')
